d1129c26029b5c3a2a88f88fd25158196707960a
Arif Billah <billaharif661@gmail.com>
2025-10-02 11:00:19 +0600

    new update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Nodemailer Transporter
 const transporter = nodemailer.createTransport({
@@ -39,6 +39,7 @@ app.post("/send-otp", async (req, res) => {
   }
 });
 
+
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
